ed0c2e35bfb9367fe2d19c5e23f9c8bae5d30d11
aiossa <iossa92@gmail.com>
2016-02-10 12:32:51 +0300

    added drawbar

--- app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -2,24 +2,34 @@ package com.qoobico.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 /**
  * Created by Aleksandr on 20.01.2016.
+ * Main activuty
  */
 public class MainActivity extends AppCompatActivity {
 
+
+    // Here we introduce our general viarables, elements
+    public static final int LAYOUT=R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
+        //we insert a toolbar to the layout
         initToolbar();
+        //we add content to navigation bar
+        initNavigationView();
+
     }
 
     private void initToolbar() {
@@ -33,7 +43,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+   }
 
-
+    //method for navigation view filling
+    private void initNavigationView() {
+    drawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
